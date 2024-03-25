251e7c44a2f4b978bdcaf11a35e9a6b43934e430
EnzoCiolin <enzopciolin@hotmail.com>
2024-03-25 14:16:31 -0300

    Arrumando o form components e criando services

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -2,24 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { GetUnitsService } from '../../services/get-units.service';
 import { Location } from '../../types/location.interface';
+import { FilterUnitsService } from '../../services/filter-units.service';
 
-const OPENNING_HOURS = {
-  morning: {
-    first: '06',
-    last: '12'
-  },
-  afternoon:{
-    first: '12',
-    last: '18',
-  },
-  night:{
-    first: '18',
-    last: '23'
-  },
 
-}
 
-type HOUR_INDEX = 'morning' | 'afternoon' | 'night'
+
 
 @Component({
   selector: 'app-forms',
@@ -30,7 +17,11 @@ export class FormsComponent {
   results: Location[] = []
   filteredResults: Location[] = []
   formGroup!: FormGroup
-  constructor(private formBuilder: FormBuilder, private unitService:GetUnitsService) {
+  constructor(
+    private formBuilder: FormBuilder, 
+    private unitService:GetUnitsService, 
+    private filterUnitsService: FilterUnitsService) 
+  {
 
   }
 
@@ -41,63 +32,15 @@ export class FormsComponent {
       showClosed: true
     })
     this.unitService.getAllUnits().subscribe(data => {
-      this.results = data.locations;
-      this.filteredResults = data.locations;
+      this.results = data;
+      this.filteredResults = data;
     })
   }
 
-  transformWeekday(weekday: number){
-    switch(weekday){
-      case 0:
-        return "Dom."
-      case 6:
-        return "Sab."
-      default:
-        return "Seg. à Sex."
-    }
-  }
-
-  filterUnits(unit: Location, open_hour:string, close_hour:string){
-    if(!unit.schedules) return true;
-    let open_hour_filter = parseInt(open_hour,10);
-    let close_hour_filter = parseInt(close_hour,10);
-
-    let todays_week_day = this.transformWeekday(new Date().getDay());
-
-    for(let i = 0; i < unit.schedules.length; i++){
-      let schedule_hour = unit.schedules[i].hour
-      let schedule_weekday = unit.schedules[i].weekdays
-      if(todays_week_day === schedule_weekday){
-        if(schedule_hour !== 'Fechada'){
-          let [unit_open_hour, unit_close_hour] = schedule_hour.split(' às ')
-          let unit_open_hour_int = parseInt(unit_open_hour.replace('h', ''), 10)
-          let unit_close_hour_int = parseInt(unit_close_hour.replace('h', ''), 10)
-
-          if(unit_open_hour_int <= open_hour_filter && unit_close_hour_int >= close_hour_filter) return true
-          else return false
-        }
-      }
-    }
-
-    return false
-  }
-
   onSubmit():void{
-
-    let intermediateResults = this.results;
-  
-    if(!this.formGroup.value.showClosed){
-      intermediateResults = this.results.filter(location => location.opened === true)
-    }
-    if(this.formGroup.value.hour){
-      const OPEN_HOUR = OPENNING_HOURS[this.formGroup.value.hour as HOUR_INDEX].first
-      const CLOSE_HOUR = OPENNING_HOURS[this.formGroup.value.hour as HOUR_INDEX].last
-      this.filteredResults = intermediateResults.filter(location => this.filterUnits(location, OPEN_HOUR, CLOSE_HOUR))
-    }else {
-      this.filteredResults = intermediateResults;
-    }
-    
-    
+    let {showClosed,hour} = this.formGroup.value
+    this.filteredResults = this.filterUnitsService.filter(this.results, showClosed, hour)
+    this.unitService.setFilterUnits(this.filteredResults)
   }
 
   onClean():void{
--- src/app/services/get-units.service.ts
+++ src/app/services/get-units.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { UnitsResponse } from '../types/units-response.interface';
+import { Location } from '../types/location.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,26 @@ export class GetUnitsService {
 
   readonly apiURl = "https://test-frontend-developer.s3.amazonaws.com/data/locations.json"
 
-  constructor(private httpClient: HttpClient) { }
+  private allUnitsSubject: BehaviorSubject<Location[]> = new BehaviorSubject<Location[]>([])
+  private allunits$: Observable<Location[]> = this.allUnitsSubject.asObservable();
+  private filteredUnits: Location[] = []
 
-  getAllUnits(): Observable<UnitsResponse>{
-    return this.httpClient.get<UnitsResponse>(this.apiURl)
+  constructor(private httpClient: HttpClient) { 
+    this.httpClient.get<UnitsResponse>(this.apiURl).subscribe(data => {
+      this.allUnitsSubject.next(data.locations);
+      this.filteredUnits = data.locations
+    })
+  }
+
+  getAllUnits(): Observable<Location[]>{
+    return this.allunits$
+  }
+
+  getFilterUnits(){
+    return this.filteredUnits
+  }
+
+  setFilterUnits(value:Location[]){
+    this.filteredUnits = value
   }
 }
